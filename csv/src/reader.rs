4ec2d42bb73aaff2f679f6df96b439b00b338bf3
Aditya Supugade <adityasupugade199344@gmail.com>
2022-10-04 00:49:09 +0530

    Add testcases for csv reader and writer.

--- csv/src/reader.rs
+++ csv/src/reader.rs
@@ -18,3 +18,64 @@ pub async fn read_csv(reader: &mut Reader) -> impl futures::Stream<Item = Result
                 .map_err(anyhow::Error::from)
         })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::fmt::Error;
+    use futures::{FutureExt, TryStreamExt};
+    use models::{logger::create_span, transactions::{Transaction, TransactionKind}};
+    use tokio_stream::StreamExt;
+
+    use super::read_csv;
+
+
+    #[test]
+    fn test_read_csv() {
+        let rt = Arc::new(models::infra::get_runtime(1, 1, create_span()).unwrap());
+        rt.block_on(run_read_csv_test())
+    }
+
+    async fn run_read_csv_test() {
+        let mut input = r"
+        type,client,tx,amount
+        deposit,1,1,2
+        deposito,1,2,2.0
+        withdrawal,1,3,5.0
+        resolve,1,4,
+        resolve,1,5, 50.0
+        dispute,1,6,
+        dispute,1,7, 50.0
+        chargeback,1,8,
+        chargeback,1,9, 100.000
+        deposit,1,10,8.4521
+        withdrawal,1,11,7.9462
+        withdrawal,1,12,
+        deposit,1,12,"
+            .as_bytes();
+
+        let result = read_csv(&mut input)
+            .map(|tx| tx.map_err(|_| Error))
+            .await
+            .collect::<Vec<_>>()
+            .await;
+
+        let expected = vec![
+            Ok(Transaction { kind: TransactionKind::Deposit, client_id: 1, id: 1, amount: Some(2.0), under_dispute: false }),
+            Err(Error),
+            Ok(Transaction { kind: TransactionKind::Withdrawal, client_id: 1, id: 3, amount: Some(5.0), under_dispute: false }),
+            Ok(Transaction { kind: TransactionKind::Resolve, client_id: 1, id: 4, amount: None, under_dispute: false }),
+            Ok(Transaction { kind: TransactionKind::Resolve, client_id: 1, id: 5, amount: Some(50.0), under_dispute: false }),
+            Ok(Transaction { kind: TransactionKind::Dispute, client_id: 1, id: 6, amount: None, under_dispute: false }),
+            Ok(Transaction { kind: TransactionKind::Dispute, client_id: 1, id: 7, amount: Some(50.0), under_dispute: false }),
+            Ok(Transaction { kind: TransactionKind::ChargeBack, client_id: 1, id: 8, amount: None, under_dispute: false }), 
+            Ok(Transaction { kind: TransactionKind::ChargeBack, client_id: 1, id: 9, amount: Some(100.0), under_dispute: false }), 
+            Ok(Transaction { kind: TransactionKind::Deposit, client_id: 1, id: 10, amount: Some(8.4521), under_dispute: false }), 
+            Ok(Transaction { kind: TransactionKind::Withdrawal, client_id: 1, id: 11, amount: Some(7.9462), under_dispute: false }), 
+            Ok(Transaction { kind: TransactionKind::Withdrawal, client_id: 1, id: 12, amount: None, under_dispute: false }), 
+            Ok(Transaction { kind: TransactionKind::Deposit, client_id: 1, id: 12, amount: None, under_dispute: false })
+        ];
+
+        assert_eq!(result, expected)
+    }
+}
--- csv/src/writer.rs
+++ csv/src/writer.rs
@@ -13,3 +13,43 @@ pub async fn write_csv(writer: &mut Writer, mut account_stream: impl futures::St
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use models::{logger::create_span, account::Account};
+    use tokio::io::BufWriter;
+
+    use crate::writer::write_csv;
+
+    
+    
+    #[test]
+    fn test_write_csv() {
+        let rt = Arc::new(models::infra::get_runtime(1, 1, create_span()).unwrap());
+        rt.block_on(run_write_csv_test())
+    }
+
+    async fn run_write_csv_test() {
+        let input = vec![
+            Account::load(1, 5.36, 1.58, false),
+            Account::load(2, 8.19, 3.08, true),
+        ];
+        
+        let account_stream = futures::stream::iter(input);
+        let mut writer = BufWriter::new(Vec::<u8>::new());
+
+        let result = write_csv(&mut writer, account_stream).await;
+
+        assert!(result.is_ok());
+
+        let buffer = writer.into_inner();
+        let csv = String::from_utf8_lossy(&buffer);
+
+        assert_eq!(
+            csv,
+            "client,available,held,total,locked\n1,5.36,1.58,6.94,false\n2,8.19,3.08,11.27,true\n"
+        );
+    }
+}
