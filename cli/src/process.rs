c180eee97895550681b1f70e5c4c9d7dcf30288c
Aditya Supugade <adityasupugade199344@gmail.com>
2022-10-05 10:08:12 +0530

    Add testcases to parallelly process multiple csv.

--- cli/src/process.rs
+++ cli/src/process.rs
@@ -19,3 +19,114 @@ pub async fn process_transactions(reader: &mut Reader, store: MemStore, writer: 
     write_csv(writer, report).await?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use futures_util::stream::FuturesUnordered;
+    use futures_util::StreamExt;
+
+    use mem_store::mem_store::MemStore;
+    use models::{logger::create_span, infra::SpannedRuntime};
+    use tokio::io::BufWriter;
+
+    use super::process_transactions;
+
+    // This tests parallelly starts multiple process_transactions csv.
+    #[test]
+    fn test_process_transactions() {
+        let rt = Arc::new(models::infra::get_runtime(1, 1, create_span()).unwrap());
+        let rtc = rt.clone();
+
+        let mut output1 = BufWriter::new(Vec::<u8>::new());
+        let mut output2 = BufWriter::new(Vec::<u8>::new());
+        let mut output3 = BufWriter::new(Vec::<u8>::new());
+        
+        rt.block_on(run_process_transactions_test(&mut output1, &mut output2, &mut output3, rtc));
+
+        let buffer = output1.into_inner();
+        let csv = String::from_utf8_lossy(&buffer);
+        
+        let expected = (csv
+            == "client,available,held,total,locked\n1,250.0,0.0,250.0,false\n2,0.0,0.0,0.0,true\n")
+            || (csv == "client,available,held,total,locked\n2,0.0,0.0,0.0,true\n1,250.0,0.0,250.0,false\n");
+
+        assert!(expected);
+
+        let buffer = output2.into_inner();
+        let csv = String::from_utf8_lossy(&buffer);
+        
+        let expected = (csv
+            == "client,available,held,total,locked\n1,250.0,0.0,250.0,false\n2,0.0,0.0,0.0,true\n")
+            || (csv == "client,available,held,total,locked\n2,0.0,0.0,0.0,true\n1,250.0,0.0,250.0,false\n");
+
+        assert!(expected);
+
+        let buffer = output3.into_inner();
+        let csv = String::from_utf8_lossy(&buffer);
+        
+        let expected = (csv
+            == "client,available,held,total,locked\n1,250.0,0.0,250.0,false\n2,0.0,0.0,0.0,true\n")
+            || (csv == "client,available,held,total,locked\n2,0.0,0.0,0.0,true\n1,250.0,0.0,250.0,false\n");
+
+        assert!(expected);
+    }
+
+    async fn run_process_transactions_test(output1: &mut BufWriter<Vec<u8>>, output2: &mut BufWriter<Vec<u8>>, output3: &mut BufWriter<Vec<u8>>, rt: Arc<SpannedRuntime>) {
+
+        let mut input1 = r"
+        type,client,tx,amount
+        deposit,1,1,100
+        withdrawal,1,2,50
+        deposit,2,3,100
+        deposit,1,4,200
+        dispute,1,4
+        resolve,1,4
+        dispute,2,3
+        chargeback,2,3
+        dispute,1,3"
+            .as_bytes();
+
+        let mut input2 = r"
+        type,client,tx,amount
+        deposit,1,1,100
+        withdrawal,1,2,50
+        deposit,2,3,100
+        deposit,1,4,200
+        dispute,1,4
+        resolve,1,4
+        dispute,2,3
+        chargeback,2,3
+        dispute,1,3"
+            .as_bytes();
+
+        let mut input3 = r"
+        type,client,tx,amount
+        deposit,1,1,100
+        withdrawal,1,2,50
+        deposit,2,3,100
+        deposit,1,4,200
+        dispute,1,4
+        resolve,1,4
+        dispute,2,3
+        chargeback,2,3
+        dispute,1,3"
+            .as_bytes();
+
+        let mut futures = FuturesUnordered::new();
+        let rtc = rt.clone();
+        let store1 = MemStore::default();
+        let store2 = MemStore::default();
+        let store3 = MemStore::default();
+
+        let fut1 = process_transactions(&mut input1, store1, output1, rtc.clone(), 2);
+        let fut2 = process_transactions(&mut input2, store2, output2, rtc.clone(), 2);
+        let fut3 = process_transactions(&mut input3, store3, output3, rtc, 2);
+        futures.push(fut1);
+        futures.push(fut2);
+        futures.push(fut3);
+
+        while let Some(_) = futures.next().await {}
+    }
+}
--- publish/src/publish.rs
+++ publish/src/publish.rs
@@ -1,12 +1,12 @@
 use models::{transactions::Transaction, error::{Error, ErrorKind}, account::Account, infra::SpannedRuntime};
-use std::{collections::HashMap, sync::{Arc, Mutex}, pin::Pin};
+use std::{collections::HashMap, sync::Arc, pin::Pin};
 
 use engine::engine::Engine;
 use mem_store::mem_store::MemStore;
-use tokio::{sync::mpsc::Sender, task::JoinHandle};
+use tokio::{sync::{mpsc::Sender, Mutex}, task::JoinHandle};
 
 pub struct Publisher {
-    map: HashMap<u16, Sender<Transaction>>,
+    client_sender_map: HashMap<u16, Sender<Transaction>>,
     mem_store: MemStore,
     rt: Arc<SpannedRuntime>,
     worker_count: u16,
@@ -15,37 +15,45 @@ pub struct Publisher {
 
 impl Publisher {
     pub fn new(mem_store: MemStore, rt: Arc<SpannedRuntime>, worker_count: u16) -> Self {
-        Self{map: HashMap::new(), mem_store, rt, worker_count, workers: Arc::new(Mutex::new(Vec::new()))}
+        Self{client_sender_map: HashMap::new(), mem_store, rt, worker_count, workers: Arc::new(Mutex::new(Vec::new()))}
     }
 
+    // Post transaction will send the given transaction on engine processing
+    // channel based on client ID sharded with worked count.
+    // Transactions for different clients will be processed parallelly,
+    // and transaction for single client will be processed sequentially.
     pub async fn post_txn(&mut self, transaction: Transaction) -> Result<(), Error> {
         
-        match self.map.get(&(&transaction.client_id % self.worker_count)) {
+        match self.client_sender_map.get(&(&transaction.client_id % self.worker_count)) {
             Some(tx) => {
                 tx.send(transaction).await?
             },
             None => {
+                // Spawn new worker.
+                tracing::info!("Spawning new payment engine worker");
                 let (tx, rx) = tokio::sync::mpsc::channel(10);
                 let worker = Engine::new(self.mem_store.clone()).start(self.rt.clone(), rx).await;
                 tx.send(transaction.clone()).await?;
-                self.workers.lock().expect("").push(worker);
-                self.map.insert(transaction.client_id % self.worker_count, tx);
+                self.workers.lock().await.push(worker);
+                self.client_sender_map.insert(transaction.client_id % self.worker_count, tx);
                 
             },
         }
         Ok(())
     }
 
+    // shutdown_gracefully will wait until all workers finish processing.
     pub async fn shutdown_gracefully(&mut self) -> Vec<Result<(), Error>> {
-        self.map.clear();
+        self.client_sender_map.clear();
         let mut results = Vec::new();
-        for worker in self.workers.lock().expect("Ignore lock poisoning")
+        for worker in self.workers.lock().await
             .iter_mut()
         {
             results.push(worker.await.map_err(|e|
                 Error::new(ErrorKind::JoinError(e))
             ));
         }
+        tracing::info!("Stopped all payment engine workers");
         return results;
     }
 
